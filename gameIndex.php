61ccbd7cd278e0ab4d6e0f33fe5d38bdd940c290
Ethan Lee <elee@training.local>
2020-11-09 09:20:53 -0500

    commit on main

---
+++ gameIndex.php
@@ -0,0 +1,9 @@
+<!--Basic Hud elements-->
+
+<!--Button to throw ball-->
+<!--WE could add a hold-the-button function to detemine how good the throw is-->
+
+<!--Random Number to determine how many pins are knocked-->
+<!--We could add another 'player' we you compete against-->
+
+<!--Score counter-->
