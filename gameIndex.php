da7f8e617282637075b6935c75bc3878ecdb9253
Peter Steiner <psteiner@training.local>
2020-11-09 14:09:17 -0500

    scorecard functional

--- gameIndex.php
+++ gameIndex.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href=''>
+    <link rel="stylesheet" href='./View/Public/gameCSS.css'>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -38,7 +38,36 @@
 
 <!--Score card-->
     <div class='score-grid'>
-
+        <!-- Frame rows -->
+            <div style='grid-column:1; grid-row: 1; display:grid; margin-right: 10px; margin-left: 10px'>
+                <!-- Name -->
+                <p style='margin: auto'>Zach</p>
+            </div>
+            <!-- Populate the table by using Javascript to write to td id's plus the index number, starting at 2 and ending at 11. -->
+            <?php //place frame grids
+                for($index = 2; $index < 12; $index++){
+                    echo"
+                        <div class='frame-grid' style='grid-column: $index'>
+                            <table class='frame-table'>
+                                <tr>
+                                    <!-- Split 1 -->
+                                    <td id='firstSplit$index'>9</td>
+                                    <!-- Split 2 -->
+                                    <td id='secondSplit$index'>/</td>
+                                </tr>
+                                <tr>
+                                    <!-- Frame Total -->
+                                    <td colspan='2' id='total$index'>10</td>
+                                </tr>
+                            </table>
+                        </div>
+                    ";
+                }
+            ?>
+            <script>
+                document.getElementById('firstSplit4').innerHTML='3';
+            </script>
+        <!-- Frame rows -->
     </div>
 <!--End game function showing who won-->
 
