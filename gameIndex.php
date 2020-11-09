92f37da770dfc1d22e708b959fee5ac3432dad50
Ethan Lee <elee@training.local>
2020-11-09 10:03:15 -0500

    commit on TestingBranch

--- gameIndex.php
+++ gameIndex.php
@@ -26,11 +26,17 @@
 
 
 <!--Button to throw ball-->
+<button id="Bowl">Knock those pins!</button>
 <!--We could add a hold-the-button function to detemine how good the throw is-->
 <!--If we have enough time, we could add a cheat function.-->
 
 
 <!--Random Number to determine how many pins are knocked-->
+<script>
+    function Bowl(){
+        var Scored = Math.floor(Math.random() * 11);
+    }
+</script>
 <!--Variable to count current number of pins before moving to next turn-->
 <!--We could add another 'player' that the player competes against-->
 <!--Do some math with the bowling rules-->
